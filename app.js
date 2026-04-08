41c20830942ada439ab3ec7242e58bd8c4fc6860
jokart-Yardbirdjo <jokart@gmail.com>
2026-04-08 12:17:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 // app.js
-import { showModal, hideModal, setMode, setSub, setPill, setLevel, renderPlaylist, renderSubPills, populateStats, setupDailyButton, buildSetupScreen } from './ui.js';
 import { state } from './state.js';
-import { showModal, hideModal, setMode, setSub, setPill, setLevel, renderPlaylist, renderSubPills, populateStats, setupDailyButton } from './ui.js';
+import { showModal, hideModal, setMode, setSub, setPill, setLevel, renderPlaylist, renderSubPills, populateStats, setupDailyButton, buildSetupScreen } from './ui.js';
 import { handleHostSetup, handleJoinScreen, createRoom, joinRoom, startMultiplayerGame, cancelLobby, cancelActiveGame, submitClientTextGuess, requestClientLifeline } from './multiplayer.js';
-//import { startDailyChallenge, startGame, handleStop, forceLifeline, evaluateGuess, resetStats, shareChallenge } from './gameLogic.js';
-import { startDailyChallenge, startGame, handleStop, forceLifeline, evaluateGuess, resetStats, shareChallenge } from './mathLogic.js';
 
+// 🎮 1. IMPORT THE CARTRIDGES
+import * as SongTrivia from './gameLogic.js';
+import * as FastMath from './mathLogic.js';
+
+// 🔌 2. THE CONSOLE SWITCH (Change to SongTrivia to play music!)
+const activeCartridge = FastMath; 
+
+// Expose UI & Multiplayer functions to HTML
 window.showModal = showModal; window.hideModal = hideModal;
 window.setMode = setMode; window.setSub = setSub; window.setPill = setPill; window.setLevel = setLevel;
 window.renderPlaylist = renderPlaylist;
-
 window.handleHostSetup = handleHostSetup; window.handleJoinScreen = handleJoinScreen;
 window.createRoom = createRoom; window.joinRoom = joinRoom;
 window.startMultiplayerGame = startMultiplayerGame; window.cancelLobby = cancelLobby;
 window.cancelActiveGame = cancelActiveGame; window.submitClientTextGuess = submitClientTextGuess;
 window.requestClientLifeline = requestClientLifeline;
 
-window.startDailyChallenge = startDailyChallenge; window.startGame = startGame;
-window.handleStop = handleStop; window.forceLifeline = forceLifeline;
-window.evaluateGuess = evaluateGuess; window.resetStats = resetStats;
-window.shareChallenge = shareChallenge;
+// 🕹️ 3. ROUTE HTML BUTTONS TO THE ACTIVE CARTRIDGE
+window.startDailyChallenge = () => activeCartridge.startDailyChallenge();
+window.startGame = () => activeCartridge.startGame();
+window.handleStop = () => activeCartridge.handleStop();
+window.forceLifeline = () => activeCartridge.forceLifeline();
+window.evaluateGuess = (isCorrect) => activeCartridge.evaluateGuess(isCorrect);
+window.resetStats = () => activeCartridge.resetStats();
+window.shareChallenge = () => activeCartridge.shareChallenge();
 
 window.onload = () => {
-    // ---- NEW LINE HERE ----
-    // Let's test the Math manifest! (Change to SongTrivia to test the other one)
-    buildSetupScreen(FastMath.manifest); 
-    //
-    renderSubPills();
+    // Tell UI to build the screen based on the active cartridge!
+    buildSetupScreen(activeCartridge.manifest);
+
+    // Only render the Era/Genre pills if we are playing Song Trivia
+    if (activeCartridge.manifest.id === 'song_trivia') {
+        renderSubPills();
+    }
+
     const todayStr = new Date().toDateString();
     if (state.userStats.lastPlayedDate !== todayStr && state.userStats.lastPlayedDate !== null) {
         state.userStats.playedDailyToday = false;
